493cc044cdf192d7870d3c713face13e0fea78b5
jeffreylozano1376 <jeffreylozano1376@yahoo.com.ph>
2020-08-19 18:49:07 +0800

    fdas

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort
 
-# flask application instance boi
+# flask application instance
 app = Flask(__name__)
 api = Api(app)
 
